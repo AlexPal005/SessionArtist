6b362bef4e8624032fa9be373d33f1409cab19d5
AlexPal005 <rudenkosanya06@gmail.com>
2023-01-01 01:09:32 +0200

    fix adaptive burger

--- js/headerAdaptive.js
+++ js/headerAdaptive.js
@@ -1,34 +1,32 @@
-$(document).ready(function (){
-    $(".header > img:nth-child(1)").on("click",function(){
-        $("#blockSearch").show();
-        $(".header > .logoBlock").hide();
-        $(".header > img").hide();
-        $(".header").css("display", "block");
-        $("#blockSearch > img:nth-child(3)").show();
-    });
 
-    $("#blockSearch > img:nth-child(3)").click(function (){
-        $("#blockSearch").hide();
-        $(".header > .logoBlock").show();
-        $(".header > img:nth-child(1),img:nth-child(3)").show();
-        $(".header").css("display", "grid");
-        $("#blockSearch > img:nth-child(3)").hide();
-    });
+$(".header > img:nth-child(1)").on("click",function(){
+    $("#blockSearch").show();
+    $(".header > .logoBlock").hide();
+    $(".header > img").hide();
+    $(".header").css("display", "block");
+    $("#blockSearch > img:nth-child(3)").show();
+});
 
-    $("#burger").click(function () {
-        $("#blockButtonsRegister").show();
-        $(".textContent, .cards, .social, .footerMain").hide();
-        $("#burger").hide();
-        $(".header > img:nth-child(4)").show();
-        $("#menuTop").show();
-    });
+$("#blockSearch > img:nth-child(3)").click(function (){
+    $("#blockSearch").hide();
+    $(".header > .logoBlock").show();
+    $(".header > img:nth-child(1),img:nth-child(3)").show();
+    $(".header").css("display", "grid");
+    $("#blockSearch > img:nth-child(3)").hide();
+});
 
-    $(".header > img:nth-child(4)").click(function () {
-        $("#blockButtonsRegister").hide();
-        $(".textContent, .cards, .social, .footerMain").show();
-        $(".header > img:nth-child(4)").hide();
-        $("#burger").show();
-        $("#menuTop").hide();
-    });
+$("#burger").click(function () {
+    $("#blockButtonsRegister").show();
+    $(".textContent, .cards, .social, .footerMain").hide();
+    $("#burger").hide();
+    $(".header > img:nth-child(4)").show();
+    $("#menuTop").show();
+});
 
+$(".header > img:nth-child(4)").click(function () {
+    $("#blockButtonsRegister").hide();
+    $(".textContent, .cards, .social, .footerMain").show();
+    $(".header > img:nth-child(4)").hide();
+    $("#burger").show();
+    $("#menuTop").hide();
 });
